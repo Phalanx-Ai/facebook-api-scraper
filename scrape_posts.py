fb3ce2e1c5ccda9fa962bab304b8bfeae0cdc07e
Marek 'marx' Grác <mgrac@redhat.com>
2024-02-02 12:07:48 +0100

    [fix] Upgrade facebook-sdk and GraphAPI version to current version
    Previous version is going to be obsolete at Feb 8 2024

--- scrape_posts.py
+++ scrape_posts.py
@@ -7,7 +7,7 @@ import facebook
 PAGE_TOKEN = os.environ['PAGE_TOKEN']
 PAGE_ID = os.environ['PAGE_ID']
 
-print(json.dumps(facebook.GraphAPI(access_token=PAGE_TOKEN, version="2.12").get_object(
+print(json.dumps(facebook.GraphAPI(access_token=PAGE_TOKEN).get_object(
     '/%s/posts' % (PAGE_ID),
     fields="id,created_time,message,permalink_url,"
         "insights.metric(post_reactions_by_type_total).period(lifetime)"
